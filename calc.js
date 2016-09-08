3ae8d7b2053c8bbd9daf41b159ebb22c791441cc
Guirerume <gpaula@2mundos.net>
2016-09-07 22:44:09 -0300

    Trying to identify equal

--- calc.js
+++ calc.js
@@ -1,5 +1,35 @@
-var addNumber = function (number)
+var mathExpression = function (expression)
 {		
-	document.getElementById('visor').value += number;
+  
+	var firstNumber = 0;
+	var conditionExpression = "";
+	var secondNumber = 0;
+	var expressionResult = null;
+   
+	document.getElementById('visor').value += expression;
+
+	if(document.getElementById("=").clicked)
+	{
+		console.log("eae");
+		for(var count = 0; count >= expression.length; count++)
+  		{
+    		if(expression[count] == "+")
+    		{
+      			
+       		for(var searchFirstnumber = 0; searchFirstnumber < count; searchFirstnumber++)
+       		{
+         		firstNumber += expression[searchFirstnumber];      
+       		} 
+       
+       		for(var searchSecondnumber = count; searchSecondnumber <= expression.length; searchSecondnumber++)
+         	{
+           		secondNumber += expression[searchSecondnumber];           
+         	}
+      		console.log(firstNumber);
+      		console.log(secondNumber);
+    	}  
+  	}
+
+  }
 }
 
